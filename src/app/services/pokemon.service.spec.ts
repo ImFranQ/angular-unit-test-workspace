e868a277c50b8999a96b94e6a379c4c04994041a
Franklin Gil <imfranq.ve@gmail.com>
2022-09-30 16:36:58 -0400

    testing for pokemon list

--- src/app/services/pokemon.service.spec.ts
+++ src/app/services/pokemon.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { PokemonService } from './pokemon.service';
 
 describe('PokemonService', () => {
-  let service: PokemonService;
+  let service: PokemonService, controller: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule]
+    });
     service = TestBed.inject(PokemonService);
+    controller = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('pokemons', () => {
+    it('pokemons list', (done) => {
+      // Arange
+      const mockData: {name:string, url: string}[] = [{
+        name: "bulbasaur",
+        url: "https://pokeapi.co/api/v2/pokemon/1/"
+      }]
+      
+      // Act
+      service.pokemons().subscribe(data => {
+        // Assert
+        expect(data).toEqual(mockData); // Test if the data is the same as the mock data
+        done() // Finish the test
+      })
+
+      // get the request to replace the mock data
+      const req = controller.expectOne('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
+      // set the mock data
+      req.flush(mockData);
+      // verify that the request is complete
+      controller.verify();
+      
+    })
+  })
 });
